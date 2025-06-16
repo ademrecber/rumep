79d375a53855dee87a2b4e81d9b639ed0eb7f91b
ademrecber <ademrecber88@gmail.com>
2025-06-17 00:33:30 +0300

    Tüm proje güncellemeleri commit edildi

--- rumep/settings.py
+++ rumep/settings.py
@@ -50,6 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'csp.middleware.CSPMiddleware',  # django-csp middleware'ini ekleyin
     'main.middleware.SecurityMiddleware',
     'main.middleware.SocialAuthExceptionMiddleware',
     'main.middleware.UserLanguageMiddleware',  # Dil için eklendi
