4041ac1a5a598bea9ef6c5637ef36508b383f4c3
ademrecber <ademrecber88@gmail.com>
2025-06-19 01:33:33 +0300

    Tüm proje güncellemeleri commit edildi

--- main/admin.py
+++ main/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
-from main.models import Post, Profile, Comment, PostVote, CommentVote, Category, Critique, CritiqueVote
+from main.models import (
+    Post, Profile, Comment, PostVote, CommentVote, 
+    Category, Critique, CritiqueVote, Kategori, Kisi,
+    Album, Sarki, Sozluk, Atasozu, Deyim, Katki,
+)
 
 admin.site.register(Post)
 admin.site.register(Profile)
@@ -9,3 +13,11 @@ admin.site.register(CommentVote)
 admin.site.register(Category)
 admin.site.register(Critique)
 admin.site.register(CritiqueVote)
+admin.site.register(Kategori)
+admin.site.register(Kisi)
+admin.site.register(Album)
+admin.site.register(Sarki)
+admin.site.register(Sozluk)
+admin.site.register(Atasozu)
+admin.site.register(Deyim)
+admin.site.register(Katki)
