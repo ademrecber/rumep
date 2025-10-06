bfec58918cf17dfbc0b2f3319ee7edeeed4ffc34
ademrecber <ademrecber88@gmail.com>
2025-10-06 20:52:07 +0300

    Tüm proje güncellemeleri commit edildi

--- rumep/settings.py
+++ rumep/settings.py
@@ -87,13 +87,15 @@ TEMPLATES = [
 WSGI_APPLICATION = 'rumep.wsgi.application'
 
 # Database
-import dj_database_url
-
 DATABASES = {
-    'default': dj_database_url.config(
-        default=os.getenv('DATABASE_URL'),
-        conn_max_age=600
-    )
+    'default': {
+        'ENGINE': os.getenv('ENGINE'),
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT'),
+    }
 }
 
 # Password validation
