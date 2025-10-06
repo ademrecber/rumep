1b3835828c196043428ee927309aedebd9f01198
ademrecber <ademrecber88@gmail.com>
2025-10-06 20:31:51 +0300

    Tüm proje güncellemeleri commit edildi

--- rumep/urls.py
+++ rumep/urls.py
@@ -1,9 +1,19 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth.views import LogoutView
+from django.http import HttpResponse
+from django.conf import settings
+import os
+
+def ads_txt_view(request):
+    ads_txt_path = os.path.join(settings.BASE_DIR, 'staticfiles', 'ads.txt')
+    with open(ads_txt_path, 'r') as f:
+        content = f.read()
+    return HttpResponse(content, content_type='text/plain')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('ads.txt', ads_txt_view, name='ads_txt'),
     path('', include('main.urls')),
     path('auth/', include('social_django.urls', namespace='social')),
     path('logout/', LogoutView.as_view(), name='logout'),
